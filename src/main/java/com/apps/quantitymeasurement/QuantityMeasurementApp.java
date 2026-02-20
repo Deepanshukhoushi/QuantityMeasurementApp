5ca49a8cc054e5e13911a3cab50b8706db1b5e11
Deepanshukhoushi <deepanshukhoushi@gmail.com>
2026-02-20 17:17:37 +0530

    [Deepanshu]: added UC2: Feet and Inches measurement equality

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -3,18 +3,37 @@ package com.apps.quantitymeasurement;
 import java.util.Scanner;
 
 public class QuantityMeasurementApp {
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		
-		System.out.println("Enter first no.(feet): ");
-		Feet first = new Feet(input.nextDouble());
-		System.out.println("Enter second no. (feet): ");
-		Feet second = new Feet(input.nextDouble());
 
-		boolean result = first.equals(second);
+    public static void main(String[] args) {
 
-		System.out.println("Input: " + first + " and " + second);
-		System.out.println("Output: Equal (" + result + ")");
-		input.close();
-	}
+        Scanner input = new Scanner(System.in);
+
+        // -------- Feet Equality --------
+        System.out.println("Enter first no. (feet): ");
+        Feet firstFeet = new Feet(input.nextDouble());
+
+        System.out.println("Enter second no. (feet): ");
+        Feet secondFeet = new Feet(input.nextDouble());
+
+        boolean feetResult = firstFeet.equals(secondFeet);
+
+        System.out.println("Input: " + firstFeet + " and " + secondFeet);
+        System.out.println("Output: Equal (" + feetResult + ")");
+
+        System.out.println("----------------------------------");
+
+        // -------- Inches Equality --------
+        System.out.println("Enter first no. (inch): ");
+        Inches firstInch = new Inches(input.nextDouble());
+
+        System.out.println("Enter second no. (inch): ");
+        Inches secondInch = new Inches(input.nextDouble());
+
+        boolean inchResult = firstInch.equals(secondInch);
+
+        System.out.println("Input: " + firstInch + " and " + secondInch);
+        System.out.println("Output: Equal (" + inchResult + ")");
+
+        input.close();
+    }
 }
